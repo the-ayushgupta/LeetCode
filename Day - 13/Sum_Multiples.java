5661cb8b4c1ab05d2c922cab9504ec6cb90b2b95
AYUSH GUPTA <118110120+the-ayushgupta@users.noreply.github.com>
2023-06-01 08:28:43 +0530

    Create Sum_Multiples.java

---
+++ Day - 13/Sum_Multiples.java
@@ -0,0 +1,13 @@
+class Sum_Multiples {
+    public int sumOfMultiples(int n) {
+        int ans = 0;
+        for(int i = 1;i <= n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+}
