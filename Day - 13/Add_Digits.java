52c2fdf42fcd81b24c0501238e8b061389a9bc35
AYUSH GUPTA <118110120+the-ayushgupta@users.noreply.github.com>
2023-06-01 08:29:17 +0530

    Create Add_Digits.java

---
+++ Day - 13/Add_Digits.java
@@ -0,0 +1,16 @@
+class Add_Digits {
+    public int addDigits(int num) {
+       int sum = 0;
+       while(num > 0 || sum > 9)
+       {
+           if(num == 0)
+           {
+               num = sum;
+               sum = 0;
+           }
+           sum += num%10;
+           num = num/10;
+       } 
+       return sum;
+    }
+}
